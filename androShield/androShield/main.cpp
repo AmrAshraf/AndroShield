b7a6a1a0adff012ddeea68ea7dba9e442ea6a3c4
DESKTOP-BP9EONJ\Amr <amr_ashraf_mohamed_amin@yahoo.com>
2018-02-06 03:04:51 +0200

    add xmlParsing feature of Nouran

--- androShield/androShield/main.cpp
+++ androShield/androShield/main.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
+#include "XmlParser.h"
 using namespace std;
 int main(int argc, const char * argv[])
 {
 
+	string relpath = "D:\\androshield\\XmlParserDebugAndBackupFeature\\test.xml";
+	XmlParser *x = new XmlParser(relpath);
+	x->getApplicationPermissions();
+	x->getComponentsPermissions();
+	x->getExportedComponents();
+	x->getActivities();
 	return 0;
 }
