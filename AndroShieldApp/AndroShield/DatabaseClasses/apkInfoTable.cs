83b27fbc81bb7b458780836a54adbeb6788f9542
MennaTMagdy <menna.tullah.magdy731@gmail.com>
2018-03-05 22:07:39 +0200

    the dll is working let's not mess it up

--- AndroShieldApp/AndroShield/DatabaseClasses/apkInfoTable.cs
+++ AndroShieldApp/AndroShield/DatabaseClasses/apkInfoTable.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Data.SqlClient;
-
+using System.Web;
+using AndroShield.Web_Forms;
+using TaintAnalysis;
+using APKInfoExtraction;
 namespace AndroShield
 {
     public class apkInfoTable
